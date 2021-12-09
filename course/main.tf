0ebfea31f8ca28cc118aabad87193ae8a34e8309
epitchi <thienvanlea1@gmail.com>
2021-12-09 09:48:14 +0700

    Modifying Infrastructure: Tags, switch to red hat

--- course/main.tf
+++ course/main.tf
@@ -14,6 +14,12 @@ provider "aws" {
 
 # Create a VPC
 resource "aws_instance" "example" {
-  ami        = "ami-0907c2c44ea451f84"
+  # ami           = "ami-0907c2c44ea451f84"
+  ami           = "ami-0d6ba217f554f6137"
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "tuts example"
+    foo  = "bar"
+  }
 }
