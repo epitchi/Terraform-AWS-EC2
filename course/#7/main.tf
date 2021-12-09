c4df50186a7a7069f5daeb09d0d8340366ee0984
epitchi <thienvanlea1@gmail.com>
2021-12-09 15:14:52 +0700

    Input Variables in Terraform

--- course/#7/main.tf
+++ course/#7/main.tf
@@ -7,38 +7,13 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
   region = "ap-southeast-1"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
 
-  tags = {
-    Name = "tuts vpc"
-  }
-}
-
-resource "aws_subnet" "web" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.0.0/16"
-
-    tags = {
-      Name = "web-subnet"
-    }
-}
-
-# Create a VPC
 resource "aws_instance" "example" {
-  # ami           = "ami-0907c2c44ea451f84"
-  ami           = "ami-0d6ba217f554f6137"
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.web.id
-  
-
-  tags = {
-    Name = "tuts example"
-    foo  = "bar"
-  }
+  ami           = "ami-0907c2c44ea451f84"
+  instance_type = var.my_instance_type
+    tags = var.instance_tags
 }
---
+++ course/#7/variables.tf
@@ -0,0 +1,12 @@
+
+variable "my_instance_type" {
+  type        = string
+  description = "My instance type"
+}
+
+variable "instance_tags" {
+  type = object({
+    Name = string
+    foo  = number
+  })
+}
